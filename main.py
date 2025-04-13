6191f6e59a653cd54de55a887eddf488ed7cbfc1
Adrian Auer <adriandavidauer@gmail.com>
2025-04-13 23:00:29 +0200

    Add basic structure with functions containing docstrings and NotImplementedError (#7)
    * Add basic structure with functions containing docstrings and NotImplementedError
    
    Fixes #4
    
    Add basic structure of the program with functions containing docstrings and returning NotImplementedError.
    
    * Add `search_videos` function to search for videos on YouTube based on a query.
    * Add `get_video_details` function to get details of a specific video on YouTube.
    * Add `download_video` function to download a specific video from YouTube.
    * Add `check_music_license` function to check the music license of a specific video on YouTube.
    * Add `get_trending_videos` function to get a list of trending videos on YouTube.
    * Add `main` function as the main entry point to run the program.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/adriandavidauer/copycat/issues/4?shareId=XXXX-XXXX-XXXX-XXXX).
    
    * Add return field to docstrings and add new functions
    
    * **Docstrings**: Add return field to docstrings of all functions to specify the YouTube Data API response code or a meaningful return type.
    * **New Functions**: Add `get_trending_videos` and `upload_video` functions with docstrings and NotImplementedError.
    * **Main Function**: Add return field to the docstring of the `main` function to specify None.

---
+++ main.py
@@ -0,0 +1,73 @@
+def search_videos(query):
+    """
+    Search for videos on YouTube based on a query.
+
+    :param query: The search query.
+    :raises: NotImplementedError
+    :return: YouTube Data API response code
+    """
+    raise NotImplementedError
+
+
+def get_video_details(video_id):
+    """
+    Get details of a specific video on YouTube.
+
+    :param video_id: The ID of the video.
+    :raises: NotImplementedError
+    :return: YouTube Data API response code
+    """
+    raise NotImplementedError
+
+
+def download_video(video_id):
+    """
+    Download a specific video from YouTube.
+
+    :param video_id: The ID of the video.
+    :raises: NotImplementedError
+    :return: YouTube Data API response code
+    """
+    raise NotImplementedError
+
+
+def check_music_license(video_id):
+    """
+    Check the music license of a specific video on YouTube.
+
+    :param video_id: The ID of the video.
+    :raises: NotImplementedError
+    :return: YouTube Data API response code
+    """
+    raise NotImplementedError
+
+
+def get_trending_videos():
+    """
+    Get a list of trending videos on YouTube.
+
+    :raises: NotImplementedError
+    :return: YouTube Data API response code
+    """
+    raise NotImplementedError
+
+
+def upload_video(video_path):
+    """
+    Upload a video to YouTube.
+
+    :param video_path: The path to the video file.
+    :raises: NotImplementedError
+    :return: YouTube Data API response code
+    """
+    raise NotImplementedError
+
+
+def main():
+    """
+    Main function to run the program.
+
+    :raises: NotImplementedError
+    :return: None
+    """
+    raise NotImplementedError
